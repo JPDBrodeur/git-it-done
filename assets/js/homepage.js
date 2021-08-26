aad9b36445ec8cbd9cd108983ecfef6120da7c1c
JPDBrodeur <jbrodeur001@gmail.com>
2021-08-26 11:02:58 -0400

    form submission handler

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,27 @@
-var getUserRepos = function() {
-    fetch('https://api.github.com/users/octocat/repos');
+var getUserRepos = function(user) {
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value - '';
+    } else {
+        alert('Please enter a GitHub username');
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
